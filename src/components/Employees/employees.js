e3e9784e21c49821131ccb0c9ab128a03516905a
Nik Romero <niklas@bootcampcentral.com>
2023-06-08 08:13:51 -0400

    uploading the base for employees, formbuilder, ...

---
+++ src/components/Employees/employees.js
@@ -0,0 +1,137 @@
+import React, { useState, useEffect} from "react";
+import Employee from "../Employee/employee";
+//import PropTypes from "prop-types";
+import EditEmployee from '../editEmployee'
+import Modal from "react-modal";
+import styles from "./employees.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { applyFilter, deleteEmployeeAsync, loadEmployeesAsync, setFilter } from "../../store/slices/employeeSlice";
+
+function Employees(props) {
+//made objects for operations
+	//axios objects
+	//const inputRef = useRef(null);
+	const dispatch = useDispatch();
+	const {employees} = useSelector(state => state.employees)
+	const {displayEmployees} = useSelector(state => state.displayEmployees)
+	//edit object
+	const [editEmployee,setEditEmployee] = useState(null);
+
+//axios/api stuff
+
+	useEffect(() => {
+		if(!employees.length)
+		{
+			dispatch(loadEmployeesAsync());
+		}
+		
+	},[dispatch,employees])
+	
+	useEffect(() => {
+		if(employees.length)
+		{
+			dispatch(applyFilter(employees));
+		}
+		
+		},[dispatch,employees])
+
+	//modal stuff
+	const handleOpenEditEmployee = (emp) =>
+	{
+		setEditEmployee(emp);
+	}
+
+	const handleCloseEditEmployee = () =>
+	{
+		setEditEmployee(null);
+	}
+
+	let editModal = null;
+
+	if(editEmployee)
+	{
+		Modal.setAppElement('body');
+		editModal = (
+			<>
+			<Modal className="modal " isOpen={true}>
+        			<EditEmployee id={editEmployee.employeeId}></EditEmployee>
+					<button onClick={() => 
+					{
+						handleCloseEditEmployee();
+					}}>close</button>
+    			</Modal>
+			</>
+		)
+	}
+
+	const checkIfNull = (val) => 
+	{
+		if(val === null)
+		{
+			return ""
+		}
+		else 
+		{
+			return val;
+		}
+	}
+//view return 
+	return (
+    <div>
+      <section className={styles.header} >
+        <h1>Employees</h1>
+		<div>
+		<button>
+			<img src="../../images/Vector" alt=""></img>
+          <p>Overview</p>
+        </button>
+		<section className={styles.searchbar}>
+			<input type="text"
+			onChange={evt => dispatch(setFilter(evt.target.value))}
+			>
+			</input>
+			<button onClick={() => dispatch(applyFilter(employees))}>
+			</button>
+		</section>
+		</div>
+      </section>
+		<section className={styles.employee}>
+			{/* table top */}
+			<p>Employee</p>
+			<p>Shifts</p>
+			<p>Job Title</p>
+			<p>department</p>
+			<p>Employee ID</p>
+			<p>Start Date</p>
+			<p>Options</p>
+		</section>
+	  <section>
+        {
+		displayEmployees.map((employee) => (
+          <Employee
+            // top level information
+            key={employee.employeeId}
+            firstName={employee.firstName}
+			lastName={employee.lastName}
+            title= {checkIfNull(employee.title)}
+            id={employee.employeeId}
+            //employment information
+            department={checkIfNull(employee.department)}
+            shift={checkIfNull(employee.shift)}
+            start={checkIfNull(employee.hireDate)}
+            //buttons for editing or deleting the employee
+            edit={() => {
+              handleOpenEditEmployee(employee);
+            }}
+            delete={() => {
+				dispatch(deleteEmployeeAsync(employee))
+			}}
+          ></Employee>
+        ))}
+        {editModal}
+      </section>
+    </div>
+  );
+}
+
+export default Employees;
